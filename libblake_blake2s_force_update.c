5fc2ac374608b554ef0a7039022ae57a67b52a89
Mattias Andrée <maandree@kth.se>
2022-02-19 17:32:58 +0100

    Add missing UNLIKELY
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- libblake_blake2s_force_update.c
+++ libblake_blake2s_force_update.c
@@ -27,7 +27,7 @@ libblake_blake2s_force_update(struct libblake_blake2s_state *state, const void *
 		 * does change the output, theoretically of the better.
 		 */
 		state->t[0] = (state->t[0] + 64) & UINT_LEAST32_C(0xFFFFffff);
-		if (state->t[0] < 64)
+		if (UNLIKELY(state->t[0] < 64))
 			state->t[1] = (state->t[1] + 1) & UINT_LEAST32_C(0xFFFFffff);
 
 		libblake_internal_blake2s_compress(state, &data[off]);
--- libblake_blake2s_update.c
+++ libblake_blake2s_update.c
@@ -10,7 +10,7 @@ libblake_blake2s_update(struct libblake_blake2s_state *state, const void *data_,
 	for (; len - off > 64; off += 64) {
 		/* See libblake_blake2s_force_update.c for optimisations notes */
 		state->t[0] = (state->t[0] + 64) & UINT_LEAST32_C(0xFFFFffff);
-		if (state->t[0] < 64)
+		if (UNLIKELY(state->t[0] < 64))
 			state->t[1] = (state->t[1] + 1) & UINT_LEAST32_C(0xFFFFffff);
 
 		libblake_internal_blake2s_compress(state, &data[off]);
